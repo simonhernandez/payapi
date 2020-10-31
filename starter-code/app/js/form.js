9cc21ab5afb6c35e20d2941aeb4e339423771437
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2020-10-31 02:22:48 -0400

    add email validation function

--- starter-code/app/js/form.js
+++ starter-code/app/js/form.js
@@ -25,18 +25,25 @@ function checkInputs(){
     if (nameValue === ''){
         setErrorFor(name, 'Name cannot be blank');
     }
+
     if (emailValue === ''){
         setErrorFor(email, 'Email cannot be blank');
+    } else if (!isValid(emailValue)){
+        setErrorFor(email, 'Email is not valid');
     }
+
     if (companyValue === ''){
         setErrorFor(company, 'Company cannot be blank');
     }
+
     if (titleValue === ''){
         setErrorFor(title, 'Title cannot be blank');
     }
+
     if (textValue === ''){
         setErrorFor(text, 'Text cannot be blank');
     }
+
 
 }
 
@@ -45,4 +52,8 @@ function setErrorFor(input, message){
     const small = formControl.querySelector('small');
     small.style.display = 'block';
     small.innerText = message;
+}
+
+function isValid(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
