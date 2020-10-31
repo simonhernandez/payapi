e79a0fd8f7903a45b125a057b399a06e328a8f0d
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2020-10-31 01:55:25 -0400

    declare variables and function

---
+++ starter-code/app/js/form.js
@@ -0,0 +1,18 @@
+// Variables
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const company = document.getElementById('company');
+const title = document.getElementById('title');
+
+// Main
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // TODO: call checkInput function
+});
+
+// Functions
+function checkInputs(){
+
+}
