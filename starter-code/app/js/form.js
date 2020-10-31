d4032031a18c4dd6394cc807e3d611a6de27b3bc
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2020-10-31 02:14:54 -0400

    add empty input validation functions

--- starter-code/app/js/form.js
+++ starter-code/app/js/form.js
@@ -4,6 +4,7 @@ const name = document.getElementById('name');
 const email = document.getElementById('email');
 const company = document.getElementById('company');
 const title = document.getElementById('title');
+const text = document.getElementById('text');
 
 // Main
 form.addEventListener('submit', e => {
@@ -19,9 +20,29 @@ function checkInputs(){
     const emailValue = email.value.trim();
     const companyValue = company.value.trim();
     const titleValue = title.value.trim();
+    const textValue = text.value.trim();
 
     if (nameValue === ''){
-        alert('error');
+        setErrorFor(name, 'Name cannot be blank');
+    }
+    if (emailValue === ''){
+        setErrorFor(email, 'Email cannot be blank');
+    }
+    if (companyValue === ''){
+        setErrorFor(company, 'Company cannot be blank');
+    }
+    if (titleValue === ''){
+        setErrorFor(title, 'Title cannot be blank');
+    }
+    if (textValue === ''){
+        setErrorFor(text, 'Text cannot be blank');
     }
 
+}
+
+function setErrorFor(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.style.display = 'block';
+    small.innerText = message;
 }
